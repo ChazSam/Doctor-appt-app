a8335dd0a6931228b629f857a0c60df1e3ff6878
Charles <Charlessammarco@gmail.com>
2024-04-09 16:44:42 -0500

    test3

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-test test 
+music library app
+
+user -create user, update user, delete user(will delete associated libraries)
+song -add song(adds for everyone)
+library -create, update name, add songs, delete library,  optional public/private
+
+1 user > many libraries
+
+many libraries <-> many songs
+
+user > library > song
+
+sections - home / library / songs 
+
+
+<!-- rating, review,  -->
+
+genres, artists, order
--- server/models.py
+++ server/models.py
@@ -1,6 +1,34 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
 
 from config import db
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+        
+    id = db.Column(db.Integer, primary_key =True)
+    username = db.Column(db.Integer, unique=True, nullable=False)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+
+class Song(db.Model, SerializerMixin):
+    __tablename = "songs"
+
+    id = db.Column(db.Integer, primary_key = True)
+    songname = db.Column(db.String,nullable=False)
+    bandname = db.Column(db.String,nullable=False)
+    url_link = db.Column(db.String)
+
+
+class Library(db.Model, SerializerMixin):
+    __tablename__ = "libraries"
+
+    __table_args__ = (db.CheckConstraint("length(title) >= 30"),)
+    
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    song_id = db.Column(db.Integer, db.ForeignKey('songs.id'))
