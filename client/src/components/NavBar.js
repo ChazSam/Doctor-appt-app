f8d3989a51b845c6632caba3d2a215fbaad610e6
Charles <Charlessammarco@gmail.com>
2024-04-14 19:57:10 -0500

    navbar added

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+
+function NavBar(){
+
+    return (
+        <nav>
+            <NavLink
+            to="/"
+            className="nav-link"
+            >Home
+            </NavLink>
+            <NavLink
+            to="/doctors"
+            className="nav-link"
+            >Doctors
+            </NavLink>
+            <NavLink
+            to="/login"
+            className="nav-link"
+            >Login
+            </NavLink>
+        </nav>
+    )
+}
---
+++ client/src/pages/Home.js
@@ -0,0 +1,6 @@
+import {useState, useEffect} from 'react';
+import NavBar from "../components/NavBar";
+
+function Home(){
+    
+}
