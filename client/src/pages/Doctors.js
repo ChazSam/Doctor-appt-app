628212773c4a4a27e02fa76d23f477e1e789177b
Charles <Charlessammarco@gmail.com>
2024-04-15 13:30:20 -0500

    updated doctor class

--- client/src/pages/Doctors.js
+++ client/src/pages/Doctors.js
@@ -13,6 +13,7 @@ function Doctors(){
             <h2>Dr. Howard</h2>
             <h2>Dr. Fine</h2>
             <h2>Dr. Howard</h2>
+            <img src='https://m.media-amazon.com/images/M/MV5BMzg1MzBjOGYtMjYyZi00YzM0LWIwMGQtMWU0NDc2ZGIxZGM1XkEyXkFqcGdeQXVyMDgyNjA5MA@@._V1_.jpg' alt='Some of the finest doctors!'></img>
         </>
     )
 }
