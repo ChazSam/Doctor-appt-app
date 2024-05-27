7cdb9d1e1783a3945f8f709317b0afc1eefd7268
Charles <Charlessammarco@gmail.com>
2024-05-26 20:17:23 -0500

    doctor reviews added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,10 +2,15 @@
 import { useOutletContext } from "react-router-dom"
 function Home(){
     
+    const {user} = useOutletContext()
+
+
+
     return(
         <>
         <form>
 
+          
             <h1>Home Page</h1>
 
             <h2>Welcome {user.first_name} to the Doctor medical system. Please login or search our doctors to find the best doctor for you!</h2>
--- client/src/pages/ListDoctors.js
+++ client/src/pages/ListDoctors.js
@@ -5,6 +5,13 @@ import { useOutletContext } from "react-router-dom"
 function ListDoctors(){
     const {listDoctors} = useOutletContext()
 
+
+    function scores(reviews){
+        if(reviews.length === 0) return "No reviews yet"
+        const totalScore = reviews.reduce((acc, review) => acc + review.score, 0)
+        return `${(totalScore / reviews.length).toFixed(1)} / 5`
+    }   
+        
     return(
         <>
 
@@ -15,6 +22,7 @@ function ListDoctors(){
                     <img src={doctor.image_url}></img>
                     <h3>Department: {doctor.department}</h3>
                     <p>{doctor.bio}</p>
+                    <p>Doctor Score: {scores(doctor.reviews)}</p>
                     <p style={{fontStyle: 'italic'}}>{doctor.tagline ? doctor.tagline : "doctor of medicine"}</p>
                 </div>
             ))}
